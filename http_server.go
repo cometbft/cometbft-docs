cfd7674c0e25428c934a33b7324a4f7410b8a623
Thane Thomson <connect@thanethomson.com>
2023-02-02 16:52:14 -0500

    Enable hacky rendering in local server
    Enables the following rendering tricks to get the docs site to work
    locally:
    
    - If a URL ends in `.md`, it automatically tries to instead render the
      same file with a `.html` suffix.
    - If a URL fails to render, it attempts to append `.html` to the
      filename.
    
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- http_server.go
+++ http_server.go
@@ -3,10 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
+type dirWithFallback struct {
+	http.Dir
+}
+
+var _ http.FileSystem = (*dirWithFallback)(nil)
+
+func (d *dirWithFallback) Open(name string) (http.File, error) {
+	if strings.HasSuffix(name, ".md") {
+		f, err := d.Dir.Open(name[:len(name)-3] + ".html")
+		if err == nil {
+			return f, nil
+		}
+	}
+
+	f, err := d.Dir.Open(name)
+	if err != nil {
+		f, err := d.Dir.Open(name + ".html")
+		if err != nil {
+			return nil, err
+		}
+		return f, nil
+	}
+	return f, nil
+}
+
 func main() {
-	h := http.FileServer(http.Dir("./_site"))
+	h := http.FileServer(&dirWithFallback{http.Dir("./_site")})
 	http.ListenAndServe(":8088", h)
 	fmt.Printf("Serving HTTP on 0.0.0.0 port 8088 ...\n")
 }
