473ff2d8f669d93154b8533808d35f5583af707c
Andy Nogueira <me@andynogueira.dev>
2023-02-22 17:13:43 -0500

    fix hooks to handle relative links multiple level (#47)

--- _plugins/convert_md.rb
+++ _plugins/convert_md.rb
@@ -1,9 +1,9 @@
 # Convert the links that point to a 'README.md' to 'index.html'
 Jekyll::Hooks.register :pages, :post_render do |doc|
-  doc.output = doc.output.gsub(/(href="\.?\.\/[^.]*)README\.md?(#(.*))?"/, '\1index.html\2"')
+  doc.output = doc.output.gsub(/(href="\.?\.?\/?\.?\.?\/?\.?\.\/[^.]*)README\.md?(#(.*))?"/, '\1index.html\2"')
 end
 
 # Convert the links that ends with '.md' to '.html'
 Jekyll::Hooks.register :pages, :post_render do |doc|
-  doc.output = doc.output.gsub(/(href="\.?\.\/[^.]*)\.md?(#(.*))?"/, '\1.html\2"')
+  doc.output = doc.output.gsub(/(href="\.?\.?\/?\.?\.?\/?\.?\.\/[^.]*)\.md?(#(.*))?"/, '\1.html\2"')
 end
